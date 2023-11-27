d63e1919fec839fa773eece58da096ec02a16ddb
ASzazX <147401344+ASzazX@users.noreply.github.com>
2023-11-27 21:54:37 +0100

    Dodano: historie transakcji do sendtransfer/
    Poprawiono:
    wallet/create
    
    TODO:
    poprawić weryfikacje usera

--- HistoriaTransakcji/models.py
+++ HistoriaTransakcji/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 
 class HistoriaTransakcji(models.Model):
-    ID_Portfela = models.ForeignKey('Portfel.Portfel', on_delete=models.CASCADE)
+    ID_PortfelaOdbiorcy = models.ForeignKey('Portfel.Portfel', on_delete=models.CASCADE, related_name='ID_PortfelaOdbiorcy')
+    ID_portfelaNadawcy = models.ForeignKey('Portfel.Portfel', on_delete=models.CASCADE, related_name='ID_portfelaNadawcy')
     Kwota = models.IntegerField()
     DataTransakcji = models.DateTimeField()
     Typ = models.CharField(max_length=100)
+    Tytul = models.CharField(max_length=100)
--- Portfel/views.py
+++ Portfel/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from decimal import Decimal
 
 import requests
@@ -6,9 +7,11 @@ from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from unicodedata import decimal
-
+from HistoriaTransakcji.models import HistoriaTransakcji
+from rest_framework.authentication import TokenAuthentication
 import Portfel.models as models
 import Portfel.serializer as PorfteSerializer
+from django.contrib.auth.models import User
 
 
 class CreateWallet(generics.CreateAPIView):
@@ -17,15 +20,20 @@ class CreateWallet(generics.CreateAPIView):
     def post(self, request, *args, **kwargs):
         try:
             user_id = request.headers['Userid']
+            user = User.objects.get(id=user_id)
             print(request.headers)
-            portfel = models.Portfel.objects.get(idKlientaUser=user_id)
-            if portfel is not None:
-                print(f"Portfel dla {user_id} juz istnieje")
+            portfel = None
+            try:
+                portfel = models.Portfel.objects.get(idKlientaUser=user)
                 return Response(status=status.HTTP_400_BAD_REQUEST)
-            else:
-                portfel = models.Portfel(idKlientaUser=user_id)
-                portfel.save()
-                return Response(status=status.HTTP_201_CREATED)
+            except:
+                if portfel is not None:
+                    print(f"Portfel dla {user_id} juz istnieje")
+                    return Response(status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    portfel = models.Portfel(idKlientaUser=User.objects.get(id=user_id), idKlientaGrupa=None, kwota=0)
+                    portfel.save()
+                    return Response(status=status.HTTP_201_CREATED)
 
         except KeyError:
             return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -43,18 +51,22 @@ class WalletList(generics.ListAPIView):
 
 class SendTransfer(generics.CreateAPIView):
     permission_classes = (IsAuthenticated,)
+    serializer_class = PorfteSerializer.PortfelSerializer
 
     def put(self, request, *args, **kwargs):
         try:
             portfel = models.Portfel.objects.get(idKlientaUser=request.headers['Userid'])
             docelowyPortfel = models.Portfel.objects.get(id=request.headers['Targetwallet'])
             pieniadze = Decimal(request.headers['Money'])
+
             print(f"Przelew z {portfel.id} do {docelowyPortfel.id} w kwocie {pieniadze}")
             with transaction.atomic():
                 docelowyPortfel.kwota += pieniadze
                 portfel.kwota -= pieniadze
                 docelowyPortfel.save()
                 portfel.save()
+                historia = HistoriaTransakcji(ID_portfelaNadawcy=portfel, ID_PortfelaOdbiorcy=docelowyPortfel, Kwota=pieniadze, Typ="Przelew", Tytul="Przelew z portfela", DataTransakcji=datetime.now())
+                historia.save()
                 return Response(status=status.HTTP_200_OK)
         except models.Portfel.DoesNotExist:
             return Response(status=status.HTTP_400_BAD_REQUEST)
